672a003bc70e4e4e398c048884cee4c1339939a6
fnhc <fnhc1234@163.com>
2017-05-07 23:25:51 +0800

    update

--- src/main/java/com/chinawiserv/dsp/dcs/dcm/util/Tests.java
+++ src/main/java/com/chinawiserv/dsp/dcs/dcm/util/Tests.java
@@ -2,19 +2,25 @@ package com.chinawiserv.dsp.dcs.dcm.util;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.chinawiserv.dsp.dcs.dcm.entity.SysUser;
+import com.google.common.base.Optional;
+import com.google.common.collect.Lists;
+import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
  * Created by Administrator on 2017/4/16.
  */
-public class Test {
-    private static Logger logger = LoggerFactory.getLogger(Test.class);
+public class Tests {
+    private static Logger logger = LoggerFactory.getLogger(Tests.class);
 
-    public static void main(String[] args) {
+    @Test
+    public void testToJSONString() {
         Map<String, Object> map = new HashMap<String, Object>();
         map.put("className", "dasfds");
         map.put("methodName", "method");
@@ -27,4 +33,35 @@ public class Test {
         logger.error(map.toString());
         logger.error(JSON.toJSONString(map));
     }
+
+    @Test
+    public void testOptional(){
+        Optional.of(2);
+
+        List<SysUser> sysUserList = Lists.newArrayList();
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }

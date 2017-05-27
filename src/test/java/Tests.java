558dbfd482172e51a79c0a75990f5de6b1626ad9
zhanf <zhanf@chinawiserv.com>
2017-05-27 19:21:53 +0800

    update

---
+++ src/test/java/Tests.java
@@ -0,0 +1,33 @@
+import com.alibaba.fastjson.JSONObject;
+import com.baomidou.mybatisplus.mapper.EntityWrapper;
+import com.baomidou.mybatisplus.plugins.Page;
+import com.baomidou.mybatisplus.toolkit.IdWorker;
+import com.chinawiserv.dsp.dcs.dcm.common.enums.Action;
+import com.chinawiserv.dsp.dcs.dcm.entity.po.system.SysLog;
+import com.chinawiserv.dsp.dcs.dcm.entity.vo.system.SysLogVo;
+import com.google.common.base.Preconditions;
+import org.apache.commons.collections.EnumerationUtils;
+import org.apache.commons.lang3.EnumUtils;
+import org.apache.commons.lang3.time.DateFormatUtils;
+import org.apache.commons.lang3.time.DateUtils;
+import org.junit.Test;
+
+import java.time.Clock;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * Created by zhanf on 2017/5/12.
+ */
+public class Tests {
+
+	@Test
+	public void testPage(){
+
+	}
+
+}
